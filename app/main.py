eef58b703254372bb6225e03c37cf0b14d5aa69c
Mystifine <iverson334308400@gmail.com>
2026-08-07 21:24:21 -0400

    Initial app setup with health endpoint

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import socket;
+import datetime;
+
+app = Flask(__name__);
+
+@app.route('/')
+def home():
+  return jsonify({
+    "message": "Resilient Deploy is alive",
+    "hostname": socket.gethostname(),
+    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  })
+
+@app.route("/health")
+def health():
+  return jsonify({
+    "status": "healthy"
+  }), 200
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=8080);
