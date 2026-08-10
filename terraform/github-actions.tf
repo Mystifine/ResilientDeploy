d480297656be36642d68e3b6ed55124c1de2a334
Mystifine <iverson334308400@gmail.com>
2026-08-09 21:01:01 -0400

    Added github actions role and policy

---
+++ terraform/github-actions.tf
@@ -0,0 +1,60 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "github_actions" {
+  name = "resilient-deploy-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+      }
+
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:Mysitifne/ResilientDeploy:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name = "resilient-deploy-github-actions-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:IntitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "ecs:UpdateService"
+        Resource = aws_ecs_service.app.id
+      },
+      {
+        Effect = "Allow"
+        Action = "ecs:DescribeServices"
+        Resource = aws_ecs_service.app.id
+      }
+    ]
+  })
+}
+
